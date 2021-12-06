1271299de4742ec3f8f9bc155a768e290ecde962
mrcormell <d.cormell@etoncollege.org.uk>
2021-12-06 11:00:35 +0000

    Initial Commit

---
+++ StructExtensions/StructExtensionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StructExtensionsApp.swift
+//  StructExtensions
+//
+//  Created by David Cormell on 06/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct StructExtensionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
